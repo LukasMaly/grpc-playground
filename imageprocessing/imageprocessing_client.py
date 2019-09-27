3359f3486da9f00c7ec5f85412c1e149f944753b
Lukáš Malý <lukasmaly@gmail.com>
2019-09-27 22:06:14 +0200

    Add image processing example
    Added image processing example where the input image is converted to
    grayscale.

---
+++ imageprocessing/imageprocessing_client.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import logging
+
+import cv2
+import grpc
+import numpy as np
+
+import imageprocessing_pb2
+import imageprocessing_pb2_grpc
+
+
+def run():
+    src = cv2.imread('lena.png')
+    height, width = src.shape[:2]
+    channels = src.shape[2]
+    data = src.tobytes()
+    cv2.imshow('Input', src)
+    cv2.waitKey(1)
+
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = imageprocessing_pb2_grpc.ImageProcessingStub(channel)
+        img = imageprocessing_pb2.Image(height=height, width=width, channels=channels, data=data)
+        response = stub.ToGrayscale(img)
+
+    dst = np.frombuffer(response.data, dtype=np.uint8)
+    dst = dst.reshape((response.height, response.width))
+
+    cv2.imshow('Output', dst)
+    cv2.waitKey()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
---
+++ imageprocessing/imageprocessing_server.py
@@ -0,0 +1,38 @@
+from concurrent import futures
+import time
+import logging
+
+import cv2
+import grpc
+import numpy as np
+
+import imageprocessing_pb2
+import imageprocessing_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class ImageProcessing(imageprocessing_pb2_grpc.ImageProcessingServicer):
+
+    def ToGrayscale(self, request, context):
+        src = np.frombuffer(request.data, dtype=np.uint8)
+        src = src.reshape((request.height, request.width, request.channels))
+        dst = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        return imageprocessing_pb2.Image(height=request.height, width=request.width, channels=1, data=dst.tobytes())
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    imageprocessing_pb2_grpc.add_ImageProcessingServicer_to_server(ImageProcessing(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
